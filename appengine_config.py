a72cde8103de8a586ae98638eea78e0757a3dd0f
Ninkigumi <ninkigumi@gmail.com>
2012-02-19 18:11:41 +0900

    delete

--- appengine_config.py
+++
@@ -1,8 +0,0 @@
-from gaesessions import SessionMiddleware
-
-# Make webapp.template use django 1.2
-webapp_django_version = '1.2'
-
-def webapp_add_wsgi_middleware(app):
-    app = SessionMiddleware(app, cookie_key="some really long string")
-    return app
